c147fce2a0aa5a8283cc51fe0532d7382abd63e4
Heli Eronen <herojonen@gmail.com>
2023-02-01 12:13:21 +0200

    Add model for Wine

--- backend/winejournal/models.py
+++ backend/winejournal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Wine(models.Model):
+    category = models.CharField(max_length=100)
+    wine_type = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    grape = models.CharField(max_length=200)
+    description = models.TextField(default='', blank=True, max_length=200)
+    rating = models.DecimalField(default=0.0, max_digits=2, decimal_places=1)
+    price = models.DecimalField(default=0.00, max_digits=6, decimal_places=2)
+
+    class Meta:
+        db_table = 'wines'
+        ordering = ['name']
